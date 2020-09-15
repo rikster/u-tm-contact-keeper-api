c3f9c36f399cc7fc6a0c05322a45eb3248097027
rikster <rhounslow@gmail.com>
2020-09-15 19:44:38 +1000

    intital express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+// test route
+app.get('/', (req, res) => res.json({msg: "Welcome to the contact keeper api..."}))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
