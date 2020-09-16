80548705378f81ea0217bc5e357ddb048aed3b66
rikster <rhounslow@gmail.com>
2020-09-16 15:48:46 +1000

    connect db update config

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-// test route
-app.get('/', (req, res) => res.json({msg: "Welcome to the contact keeper api..."}))
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
