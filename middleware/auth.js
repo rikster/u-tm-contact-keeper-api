0e49bdc1ad5f1dfe7224db35df264807bba45733
Richard Hounslow <rhounslow@gmail.com>
2020-10-06 15:04:58 +1100

    Small comments and updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+//next is required to move onto next middleware func
 module.exports = function(req, res, next) {
 	// Get token from header
 	const token = req.header('x-auth-token');
 
 	// Check if not token
+	// 401 - unauthorized
 	if (!token) {
 		return res.status(401).json({ msg: 'No token, authorization denied' });
 	}
 
-	// Verify token
+	// if there is a token -> verify token
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
 
