1f5738fadd5fa84a30066ced91a2f2da4480d5fe
Richard Hounslow <rhounslow@gmail.com>
2020-10-09 14:09:12 +1100

    Major commit of files

--- config/db.js
+++ config/db.js
@@ -3,15 +3,15 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-			useUnifiedTopology: true
-		});
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
 
-		console.log('MongoDB Connected...');
+    console.log('MongoDB Connected...');
 	} catch (err) {
 		console.error(err.message);
 		// Exit process with failure
--- models/Contact.js
+++ models/Contact.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const ContactSchema = new Schema({
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'users'
-	},
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String
-	},
-	phone: {
-		type: String
-	},
-	type: {
-		type: String,
-		default: 'personal' //vs professional
-	},
-	date: {
-		type: Date,
-		default: Date.now
-	}
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('contact', ContactSchema);
